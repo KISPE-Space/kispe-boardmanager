1422b5786f69e9dbf689fc26facdcd16b54db7ff
Bo Dickinson <bdickinson@kispe.co.uk>
2024-12-16 12:15:40 +0000

    Migrating this repo from https://github.com/KISPE-Harvey/kispe-boardmanager/

---
+++ README.md
@@ -0,0 +1,33 @@
+# KISPE Arduino Board Manager
+
+## STM32 
+based upon the stm32duino core supporting custom board profile for KISPE space boards and test equipment.
+
+[stm32duino GitHub Page](https://github.com/stm32duino)
+
+### Supported Boards
+## Nucleo Boards
+
+- Nucleo-H753ZIT
+
+<img src="https://github.com/KISPE-Harvey/kispe-boardmanager/blob/main/assets/img/nucleo-h753zit.png" width = 30% height = 30%>
+
+## OSSAT DEV Board
+- STM32H753ZIT
+
+<img src="https://github.com/KISPE-Harvey/kispe-boardmanager/blob/main/assets/img/ossat-dev.png" width = 30% height = 30%>
+
+
+#### SATLL
+- STM32H753VIT6
+
+<img src="https://github.com/KISPE-Harvey/kispe-boardmanager/blob/main/assets/img/stm32h7vit.jpg" width = 30% height = 30%>
+
+
+## RP2040
+
+based upon - LINK
+
+### SATLL Motor Board
+- RP4040
+<img src="https://github.com/KISPE-Harvey/kispe-boardmanager/blob/main/assets/img/rp2040.png" width = 30% height = 30%>
---
+++ stm32/0.0.1/cmake/boards_db.cmake
@@ -0,0 +1,249 @@
+# NUCLEO_H753ZI
+#H753ZIT
+# -----------------------------------------------------------------------------
+
+#set(NUCLEO_H753ZI_VARIANT_PATH "${CMAKE_CURRENT_LIST_DIR}/../variants/STM32H7xx/H742Z(G-I)T_H743Z(G-I)T_H747A(G-I)I_H747I(G-I)T_H750ZBT_H753ZIT_H757AII_H757IIT")
+set(NUCLEO_H753ZI_VARIANT_PATH "${CMAKE_CURRENT_LIST_DIR}/../variants/Nucleo_H753ZI")
+set(NUCLEO_H753ZI_MAXSIZE 2097152)
+set(NUCLEO_H753ZI_MAXDATASIZE 524288)
+set(NUCLEO_H753ZI_MCU cortex-m7)
+set(NUCLEO_H753ZI_FPCONF "fpv4-sp-d16-hard")
+add_library(NUCLEO_H753ZI INTERFACE)
+target_compile_options(H753ZITINTERFACE
+  "SHELL:-DSTM32H753xx  "
+  "SHELL:"
+  "SHELL:"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${NUCLEO_H753ZI_MCU}
+)
+target_compile_definitions(NUCLEO_H753ZI INTERFACE
+  "STM32H7xx"
+	"ARDUINO_NUCLEO_H753ZI"
+	"BOARD_NAME=\"NUCLEO_H753ZI\""
+	"BOARD_ID=NUCLEO_H753ZI"
+	"VARIANT_H=\"variant_NUCLEO_H753ZI.h\""
+)
+target_include_directories(NUCLEO_H753ZI INTERFACE
+  ${CMAKE_CURRENT_LIST_DIR}/../system/STM32H7xx
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32H7xx_HAL_Driver/Inc
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32H7xx_HAL_Driver/Src
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32H7xx/Include/
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32H7xx/Source/Templates/gcc/
+  ${NUCLEO_H753ZI_VARIANT_PATH}
+)
+
+target_link_options(NUCLEO_H753ZI INTERFACE
+  "LINKER:--default-script=${NUCLEO_H753ZI_VARIANT_PATH}/ldscript.ld"
+  "LINKER:--defsym=LD_FLASH_OFFSET=0x0"
+	"LINKER:--defsym=LD_MAX_SIZE=2097152"
+	"LINKER:--defsym=LD_MAX_DATA_SIZE=524288"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${NUCLEO_H753ZI_MCU}
+)
+
+add_library(NUCLEO_H753ZI_serial_disabled INTERFACE)
+target_compile_options(NUCLEO_H753ZI_serial_disabled INTERFACE
+  "SHELL:"
+)
+add_library(NUCLEO_H753ZI_serial_generic INTERFACE)
+target_compile_options(NUCLEO_H753ZI_serial_generic INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED"
+)
+add_library(NUCLEO_H753ZI_serial_none INTERFACE)
+target_compile_options(NUCLEO_H753ZI_serial_none INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED -DHWSERIAL_NONE"
+)
+add_library(NUCLEO_H753ZI_usb_CDC INTERFACE)
+target_compile_options(NUCLEO_H753ZI_usb_CDC INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC -DDISABLE_GENERIC_SERIALUSB"
+)
+add_library(NUCLEO_H753ZI_usb_CDCgen INTERFACE)
+target_compile_options(NUCLEO_H753ZI_usb_CDCgen INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC"
+)
+add_library(NUCLEO_H753ZI_usb_HID INTERFACE)
+target_compile_options(NUCLEO_H753ZI_usb_HID INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_HID_COMPOSITE"
+)
+add_library(NUCLEO_H753ZI_usb_none INTERFACE)
+target_compile_options(NUCLEO_H753ZI_usb_none INTERFACE
+  "SHELL:"
+)
+add_library(NUCLEO_H753ZI_xusb_FS INTERFACE)
+target_compile_options(NUCLEO_H753ZI_xusb_FS INTERFACE
+  "SHELL:"
+)
+add_library(NUCLEO_H753ZI_xusb_HS INTERFACE)
+target_compile_options(NUCLEO_H753ZI_xusb_HS INTERFACE
+  "SHELL:-DUSE_USB_HS"
+)
+add_library(NUCLEO_H753ZI_xusb_HSFS INTERFACE)
+target_compile_options(NUCLEO_H753ZI_xusb_HSFS INTERFACE
+  "SHELL:-DUSE_USB_HS -DUSE_USB_HS_IN_FS"
+)
+
+# GENERIC_H753ZITX
+# -----------------------------------------------------------------------------
+
+set(GENERIC_H753ZITX_VARIANT_PATH "${CMAKE_CURRENT_LIST_DIR}/../variants/OSSAT")
+
+
+set(GENERIC_H753ZITX_MAXSIZE 2097152)
+set(GENERIC_H753ZITX_MAXDATASIZE 524288)
+set(GENERIC_H753ZITX_MCU cortex-m7)
+set(GENERIC_H753ZITX_FPCONF "-")
+add_library(GENERIC_H753ZITX INTERFACE)
+target_compile_options(GENERIC_H753ZITX INTERFACE
+  "SHELL:-DCORE_CM7 -DSTM32H753xx  "
+  "SHELL:"
+  "SHELL:"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${GENERIC_H753ZITX_MCU}
+)
+target_compile_definitions(GENERIC_H753ZITX INTERFACE
+  "STM32H7xx"
+	"ARDUINO_GENERIC_H753ZITX"
+	"BOARD_NAME=\"GENERIC_H753ZITX\""
+	"BOARD_ID=GENERIC_H753ZITX"
+	"VARIANT_H=\"variant_STM32H753ZIT.h\""
+)
+target_include_directories(GENERIC_H753ZITX INTERFACE
+  ${CMAKE_CURRENT_LIST_DIR}/../system/STM32H7xx
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32H7xx_HAL_Driver/Inc
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32H7xx_HAL_Driver/Src
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32H7xx/Include/
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32H7xx/Source/Templates/gcc/
+  ${GENERIC_H753ZITX_VARIANT_PATH}
+)
+
+target_link_options(GENERIC_H753ZITX INTERFACE
+  "LINKER:--default-script=${GENERIC_H753ZITX_VARIANT_PATH}/ldscript.ld"
+  "LINKER:--defsym=LD_FLASH_OFFSET=0x0"
+	"LINKER:--defsym=LD_MAX_SIZE=2097152"
+	"LINKER:--defsym=LD_MAX_DATA_SIZE=524288"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${GENERIC_H753ZITX_MCU}
+)
+
+add_library(GENERIC_H753ZITX_serial_disabled INTERFACE)
+target_compile_options(GENERIC_H753ZITX_serial_disabled INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_H753ZITX_serial_generic INTERFACE)
+target_compile_options(GENERIC_H753ZITX_serial_generic INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED"
+)
+add_library(GENERIC_H753ZITX_serial_none INTERFACE)
+target_compile_options(GENERIC_H753ZITX_serial_none INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED -DHWSERIAL_NONE"
+)
+add_library(GENERIC_H753ZITX_usb_CDC INTERFACE)
+target_compile_options(GENERIC_H753ZITX_usb_CDC INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC -DDISABLE_GENERIC_SERIALUSB"
+)
+add_library(GENERIC_H753ZITX_usb_CDCgen INTERFACE)
+target_compile_options(GENERIC_H753ZITX_usb_CDCgen INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC"
+)
+add_library(GENERIC_H753ZITX_usb_HID INTERFACE)
+target_compile_options(GENERIC_H753ZITX_usb_HID INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_HID_COMPOSITE"
+)
+add_library(GENERIC_H753ZITX_usb_none INTERFACE)
+target_compile_options(GENERIC_H753ZITX_usb_none INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_H753ZITX_xusb_FS INTERFACE)
+target_compile_options(GENERIC_H753ZITX_xusb_FS INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_H753ZITX_xusb_HS INTERFACE)
+target_compile_options(GENERIC_H753ZITX_xusb_HS INTERFACE
+  "SHELL:-DUSE_USB_HS"
+)
+add_library(GENERIC_H753ZITX_xusb_HSFS INTERFACE)
+target_compile_options(GENERIC_H753ZITX_xusb_HSFS INTERFACE
+  "SHELL:-DUSE_USB_HS -DUSE_USB_HS_IN_FS"
+)
+
+# GENERIC_H753VITX
+# -----------------------------------------------------------------------------
+
+set(GENERIC_H753VITX_VARIANT_PATH "${CMAKE_CURRENT_LIST_DIR}/../variants/SATLL")
+set(GENERIC_H753VITX_MAXSIZE 2097152)
+set(GENERIC_H753VITX_MAXDATASIZE 524288)
+set(GENERIC_H753VITX_MCU cortex-m7)
+set(GENERIC_H753VITX_FPCONF "-")
+add_library(GENERIC_H753VITX INTERFACE)
+target_compile_options(GENERIC_H753VITX INTERFACE
+  "SHELL:-DCORE_CM7 -DSTM32H753xx  "
+  "SHELL:"
+  "SHELL:"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${GENERIC_H753VITX_MCU}
+)
+target_compile_definitions(GENERIC_H753VITX INTERFACE
+  "STM32H7xx"
+	"ARDUINO_GENERIC_H753VITX"
+	"BOARD_NAME=\"GENERIC_H753VITX\""
+	"BOARD_ID=GENERIC_H753VITX"
+	"VARIANT_H=\"variant_STM32H753VIT.h\""
+)
+target_include_directories(GENERIC_H753VITX INTERFACE
+  ${CMAKE_CURRENT_LIST_DIR}/../system/STM32H7xx
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32H7xx_HAL_Driver/Inc
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/STM32H7xx_HAL_Driver/Src
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32H7xx/Include/
+  ${CMAKE_CURRENT_LIST_DIR}/../system/Drivers/CMSIS/Device/ST/STM32H7xx/Source/Templates/gcc/
+  ${GENERIC_H753VITX_VARIANT_PATH}
+)
+
+target_link_options(GENERIC_H753VITX INTERFACE
+  "LINKER:--default-script=${GENERIC_H753VITX_VARIANT_PATH}/ldscript.ld"
+  "LINKER:--defsym=LD_FLASH_OFFSET=0x0"
+	"LINKER:--defsym=LD_MAX_SIZE=2097152"
+	"LINKER:--defsym=LD_MAX_DATA_SIZE=524288"
+  "SHELL:-mfpu=fpv4-sp-d16 -mfloat-abi=hard"
+  -mcpu=${GENERIC_H753VITX_MCU}
+)
+
+add_library(GENERIC_H753VITX_serial_disabled INTERFACE)
+target_compile_options(GENERIC_H753VITX_serial_disabled INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_H753VITX_serial_generic INTERFACE)
+target_compile_options(GENERIC_H753VITX_serial_generic INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED"
+)
+add_library(GENERIC_H753VITX_serial_none INTERFACE)
+target_compile_options(GENERIC_H753VITX_serial_none INTERFACE
+  "SHELL:-DHAL_UART_MODULE_ENABLED -DHWSERIAL_NONE"
+)
+add_library(GENERIC_H753VITX_usb_CDC INTERFACE)
+target_compile_options(GENERIC_H753VITX_usb_CDC INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC -DDISABLE_GENERIC_SERIALUSB"
+)
+add_library(GENERIC_H753VITX_usb_CDCgen INTERFACE)
+target_compile_options(GENERIC_H753VITX_usb_CDCgen INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_CDC"
+)
+add_library(GENERIC_H753VITX_usb_HID INTERFACE)
+target_compile_options(GENERIC_H753VITX_usb_HID INTERFACE
+  "SHELL:-DUSBCON  -DUSBD_VID=0x0483 -DUSBD_PID=0x5740 -DHAL_PCD_MODULE_ENABLED -DUSBD_USE_HID_COMPOSITE"
+)
+add_library(GENERIC_H753VITX_usb_none INTERFACE)
+target_compile_options(GENERIC_H753VITX_usb_none INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_H753VITX_xusb_FS INTERFACE)
+target_compile_options(GENERIC_H753VITX_xusb_FS INTERFACE
+  "SHELL:"
+)
+add_library(GENERIC_H753VITX_xusb_HS INTERFACE)
+target_compile_options(GENERIC_H753VITX_xusb_HS INTERFACE
+  "SHELL:-DUSE_USB_HS"
+)
+add_library(GENERIC_H753VITX_xusb_HSFS INTERFACE)
+target_compile_options(GENERIC_H753VITX_xusb_HSFS INTERFACE
+  "SHELL:-DUSE_USB_HS -DUSE_USB_HS_IN_FS"
+)
